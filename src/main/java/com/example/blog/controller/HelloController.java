9aa77af5e96914f8f7e25856b347a9d4c828b751
高琪 <gaoqi198@163.com>
2024-01-20 22:59:45 +0800

    feat: log配置

--- src/main/java/com/example/blog/controller/HelloController.java
+++ src/main/java/com/example/blog/controller/HelloController.java
@@ -1,13 +1,22 @@
 package com.example.blog.controller;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Log4j2
 public class HelloController {
 
     @GetMapping("hello/say")
     public String Say() {
         return "Hello World! 这是我创建的第一个SpringBoot项目。";
+    }
+
+    @GetMapping("hello/log/{id}")
+    public String Log(@PathVariable("id") String id) {
+        log.info("message");
+        return id;
     }
 }
