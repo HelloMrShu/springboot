6624aa45b904ed70e661abaca8204d8815c284f2
高琪 <gaoqi198@163.com>
2024-01-21 18:01:16 +0800

    feat: crontab

---
+++ src/main/java/com/example/blog/job/TestJob.java
@@ -0,0 +1,24 @@
+package com.example.blog.job;
+
+import lombok.extern.log4j.Log4j2;
+import org.apache.ibatis.executor.ExecutorException;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+@Log4j2
+public class TestJob {
+
+    @Scheduled(cron = "0 * * * * ?")
+    @Async
+    public void run() {
+        try {
+            log.info("test cron job");
+        } catch (Exception e) {
+            log.error(e.toString());
+        }
+    }
+}
